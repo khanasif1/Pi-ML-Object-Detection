0baa2dd16d481b9ca93ebc8481897741076a744a
khanasif1 <khanasif1@gmail.com>
2022-01-14 22:57:09 +1100

    added venv

--- cvImageRead.py
+++ cvImageRead.py
@@ -1,7 +1,20 @@
 #https://www.youtube.com/watch?v=qCR2Weh64h4&list=PLzMcBGfZo4-lUA8uGjeXhBUUzPYc6vZRn
+from operator import truediv
 import cv2
+import numpy as np
 
-img = cv2.imread('assets\person.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Image', img)
-cv2.waitKey(0)
+# img = cv2.imread('assets\person.jpg', cv2.IMREAD_GRAYSCALE)
+# cv2.imshow('Image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
 cv2.destroyAllWindows()
